4ff3baf18bd363664ea97985b209f7680a4ed34a
Joel <35474476+JoelMite@users.noreply.github.com>
2025-06-26 03:35:21 -0500

    Design and Presentation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [
+    // comentamos o quitamos la integración:
+    // toolbar(),
+  ],
+vite: {
+    plugins: [tailwindcss()],
+  },
+
+});
